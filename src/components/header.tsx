bfedc4ef2efa55ceedf64d1480f876c6d4103b5d
Eraze86 <therese.rosvall@hotmail.se>
2022-09-19 20:38:26 +0200

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+export function Header(){
+    return(<>
+
+    <header>
+        <nav>
+            <ul>
+                <li>
+                       <a>Länk</a>
+                </li>
+                <li>
+                       <a>Länk</a>
+                </li>
+                <li>
+                       <a>Länk</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    </>)
+}
